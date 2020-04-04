612b1655bb0cfb700c1ba61143912b7055b08d70
Sergei Sharifov <2017ok@gmail.com>
2020-04-04 15:50:07 +0200

    JS-01

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -1,2 +1,17 @@
-"use strict" /* использование последней  версии  js  */;
+'use strict' /* использование последней  версии  js  */;
 
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message;
+
+const password = prompt('Введите пароль.');
+
+if (password === null) {
+  message = 'Отменено пользователем !'; // проверка  на  отказ
+} else {
+  if (password === ADMIN_PASSWORD) {
+    message = 'Добро пожаловать !';
+  } else {
+    message = 'Доступ запрещен, неверный пароль !';
+  }
+}
+alert(message); //  вывод  символов
