2ca28be76eee929f7a0e8e0102111d0ac5f1efba
Sergei Sharifov <2017ok@gmail.com>
2020-04-04 22:17:06 +0200

    JS-01

--- src/js/task-6.js
+++ src/js/task-6.js
@@ -1,2 +1,21 @@
-"use strict" /* использование последней  версии  js  */;
+'use strict' /* использование последней  версии  js  */;
 
+let total = 0;
+let message = 'Введите сумму.';
+let ordered;
+
+do {
+  ordered = prompt(message); //  ввод  символов
+  if (ordered === null) {
+    message = 'Введите сумму.';
+    alert(`Общяя сумма чисел равна ${total}`); //  вывод  символов
+  } else {
+    ordered = Number(ordered); //  приобразование в числ
+    if (ordered <= 0 || Number.isNaN(ordered)) {
+      message = 'Было введенно не число, попробуйте ище раз.';
+    } else {
+      total = total + ordered;
+      message = 'Введите сумму.';
+    }
+  }
+} while (1 < 20);
